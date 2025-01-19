a2397b38fc86b31949fc6692d36644f4d7b047bc
Alexander Taffe <aetaffe@ucdavis.edu>
2025-01-18 17:33:37 -0800

    updated util files

--- util/copy_seg_files.py
+++ util/copy_seg_files.py
@@ -3,19 +3,21 @@ import os
 from pathlib import Path
 import shutil
 
+print('Copying segmentation files...')
+base_folder = '/media/alex/1TBSSD/SSD/FLImDataset/'
 file_counter = 0
-folders = [f for f in glob.glob("data/train/*")]
-Path("data/segmentation_data").mkdir(parents=True, exist_ok=True)
+folders = [f for f in glob.glob(base_folder + "images/*")]
+Path(base_folder + "segmentation_data").mkdir(parents=True, exist_ok=True)
 
 for folder in folders:
-    files = [f for f in glob.glob(folder + "/*.jpg")]
-    for img_file_path in files:
-        filename = os.path.basename(img_file_path)
-        new_img_filename = f"data/segmentation_data/{file_counter}.jpg"
-        new_json_filename = f"data/segmentation_data/{file_counter}.json"
+    img_files = [f for f in glob.glob(f'{folder}/*.jpg')]
+    for img_file_path in img_files:
+        new_img_filename = f"{base_folder}/images/{file_counter}.jpg"
+        new_json_filename = f"{base_folder}/images/{file_counter}.json"
         json_file_path = img_file_path.replace('.jpg', '.json')
         if os.path.exists(json_file_path):
             shutil.move(img_file_path, new_img_filename)
             shutil.move(json_file_path, new_json_filename)
             file_counter += 1
             print(f"Moved {img_file_path} to {new_img_filename}")
+print('Done')
--- util/resize_images.py
+++ util/resize_images.py
@@ -3,7 +3,7 @@ import cv2
 from pathlib import Path
 
 
-base_folder = 'snapshots/generated'
+base_folder = '/media/alex/1TBSSD/SSD/FLImDataset/FLIm-Images-256x256'
 
 if __name__ == '__main__':
     print('Resizing images...')
@@ -11,6 +11,7 @@ if __name__ == '__main__':
     Path(f'{base_folder}/resized_images').mkdir(parents=True, exist_ok=True)
     for img_file in img_files:
         img = cv2.imread(img_file)
-        img = cv2.resize(img, (1080, 720))
+        img = cv2.resize(img, (256, 256))
         filename = f'{base_folder}/resized_images/{img_file.split("/")[-1]}'
         cv2.imwrite(filename, img)
+print('Done!')
